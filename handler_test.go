9e1295d0931538ea07dcdce5d55e236503215f28
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-16 00:24:09 +0700

    Add handler test

---
+++ handler_test.go
@@ -0,0 +1,22 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
+
+func TestHandlerServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Errorf("Server failed to start: %s", err)
+	}
+}
