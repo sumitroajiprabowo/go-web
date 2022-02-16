cb02ab10362daa6c73752902bcd022b2e6c3c0cd
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-17 00:45:24 +0700

    Add file server test

---
+++ file_server_test.go
@@ -0,0 +1,66 @@
+package golang_web
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+	"testing"
+)
+
+// Create Test Function for FileServer
+func TestFileServer(t *testing.T) {
+
+	directory := http.Dir("./resources")
+	// fileServer := FileServer(directory)
+	fileServer := http.FileServer(directory)
+
+	// Create mux
+	mux := http.NewServeMux()
+
+	// localhost:8080/resources/static/filename
+	// mux.Handle("/static/", fileServer)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Create server
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	// Start server
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Errorf("Server failed to start: %s", err)
+	}
+
+}
+
+//go:embed resources
+var resources embed.FS
+
+func TestFileServerEmbed(t *testing.T) {
+
+	directory, _ := fs.Sub(resources, "resources")
+	fileServer := http.FileServer(http.FS(directory))
+
+	// Create mux
+	mux := http.NewServeMux()
+
+	// localhost:8080/resources/static/filename
+	// mux.Handle("/static/", fileServer)
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Create server
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	// Start server
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Errorf("Server failed to start: %s", err)
+	}
+
+}
