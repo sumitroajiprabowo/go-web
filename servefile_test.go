b1c882c81c1460dd7f857b3ac2b585081b79b0d7
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-17 00:45:16 +0700

    Add servefile test

---
+++ servefile_test.go
@@ -0,0 +1,58 @@
+package golang_web
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Query().Get("name") != "" {
+		http.ServeFile(w, r, "./resources/index.html")
+	} else {
+		http.ServeFile(w, r, "./resources/not_found.html")
+	}
+}
+
+func TestServeFile(t *testing.T) {
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Errorf("Server failed to start: %s", err)
+	}
+}
+
+//go:embed resources/index.html
+var resourcesOK string
+
+//go:embed resources/not_found.html
+var resourcesNotFound string
+
+func ServeFileEmbed(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Query().Get("name") != "" {
+		fmt.Fprint(w, resourcesOK)
+	} else {
+		fmt.Fprint(w, resourcesNotFound)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		t.Errorf("Server failed to start: %s", err)
+	}
+}
