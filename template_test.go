30ace81474a8ff4cd01874d6c6ac838f2cecdc21
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-17 12:48:05 +0700

    Add template_test.go

---
+++ template_test.go
@@ -0,0 +1,112 @@
+package golang_web
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func SimpleHtml(w http.ResponseWriter, r *http.Request) {
+
+	templateText := `<html><body><h1>{{.}}</h1></body></html>`
+
+	// t, err := template.New("simple").Parse(templateText)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	t := template.Must(template.New("simple").Parse(templateText))
+
+	err := t.ExecuteTemplate(w, "simple", "Hello World")
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func TestSimpleHtml(t *testing.T) {
+
+	// request httptest
+	request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+
+	// recorder httptest
+	recorder := httptest.NewRecorder()
+
+	// handler
+	handler := http.HandlerFunc(SimpleHtml)
+
+	// serve
+	handler.ServeHTTP(recorder, request)
+
+	//response
+	response := recorder.Result()
+
+	// check body
+	body, _ := ioutil.ReadAll(response.Body)
+
+	//expected
+	expected := `<html><body><h1>Hello World</h1></body></html>`
+
+	assert.Equal(t, expected, string(body))
+
+	fmt.Println(string(body))
+
+	fmt.Println("Success")
+}
+
+func SimpleHtmlTemplate(w http.ResponseWriter, r *http.Request) {
+
+	t := template.Must(template.ParseFiles("./templates/example.gohtml"))
+
+	err := t.ExecuteTemplate(w, "example.gohtml", "Hello From HTML Template")
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func TestSimpleHtmlTemplate(t *testing.T) {
+
+	// handler
+	handler := http.HandlerFunc(SimpleHtmlTemplate)
+
+	//request
+	request := httptest.NewRequest("GET", "http://localhost:8080", nil)
+
+	// recorder
+	recorder := httptest.NewRecorder()
+
+	// serve
+	handler.ServeHTTP(recorder, request)
+
+	//response
+	response := recorder.Result()
+
+	// check body
+	body, _ := ioutil.ReadAll(response.Body)
+
+	//expected
+	expected := `<html><body><h1>Hello From HTML Template</h1></body></html>`
+
+	assert.Equal(t, expected, string(body))
+
+	fmt.Println(string(body))
+
+	fmt.Println("Success")
+}
+
+func TemplateDirectory(w http.ResponseWriter, r *http.Request) {
+
+	t := template.Must(template.ParseGlob("./templates/*.gohtml"))
+
+	err := t.Execute(w, "Hello From Template Directory")
+	if err != nil {
+		panic(err)
+	}
+
+}
